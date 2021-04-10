3bb0817ed0ba5da3558f27da815219b5e882e569
Crystal Gong <cyblackwing@hotmail.com>
2021-04-10 10:06:25 +0800

    RWD

--- media_taste/common/js/main.js
+++ media_taste/common/js/main.js
@@ -2,5 +2,32 @@ $(function () {
   $(document).scroll(function () {
     var $nav = $(".fixed-top");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
+
+    if ($(this).scrollTop() > $nav.height()) {
+      $('.top').fadeIn(500);
+    } else {
+      $('.top').fadeOut(500);
+    }
+  })
 });
+
+$(".top").click(function () {
+  $(window).scrollTop(0);
+});
+
+$('.moblie-menu-img').click(function () {
+  if ($(this).hasClass('is-open')) {
+    $(this).removeClass('is-open');
+  }
+  else {
+    $(this).addClass('is-open')
+  }
+
+  if ($('.mobile-menu').hasClass('is-open')) {
+    $('.mobile-menu').removeClass('is-open');
+  }
+  else {
+    $('.mobile-menu').addClass('is-open')
+  }
+
+})
